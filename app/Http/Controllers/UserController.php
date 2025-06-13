e770130997588357638e7df94a939fdb534020ab
dilusha07 <dilushasandaruwani07@gmail.com>
2025-06-13 10:24:21 +0530

    Find user by ID

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\UserModel;
 
 class UserController extends Controller
 {
@@ -27,7 +28,7 @@ class UserController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $user = $this->user->find($id);  
     }
 
     /**
